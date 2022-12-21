0a6e01ace72f1b8f4524ff1fa5bad57a2b58bed4
Alex <addarts98@gmail.com>
2022-12-21 17:59:04 +0200

    HomeWork 5. Commit with 2 new lections.

---
+++ Git_com/src/Homework_5/Lection_4.java
@@ -0,0 +1,9 @@
+package Homework_5;
+
+
+public class Lection_4 {
+    private final static int id = 1_004;
+    public static int amountLection_4 = 1;
+    public static final String name = "Java_4";
+    final Lection_4 lection_4 = new Lection_4();
+}
